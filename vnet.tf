f69eac3b5563716a0984b47c8d6498eb937d1143
Gerry Green <gerryg80@gmail.com>
2020-08-26 07:14:41 -0400

    checkpoint before adding to github

--- vnet.tf
+++ vnet.tf
@@ -6,16 +6,16 @@ resource "azurerm_virtual_network" "sandbox_vnet" {
 #  dns_servers         = ["10.192.0.4","10.192.0.5"]
 }
 
-resource "azurerm_network_security_group" "sandboxnsg" {
-  name                = "sandboxNSG"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.aks-sandbox.name
-}
-
 resource "azurerm_subnet" "sandbox_subnet" {
   name                 = "AzureFirewallSubnet"
   resource_group_name  = azurerm_resource_group.aks-sandbox.name
   virtual_network_name = azurerm_virtual_network.sandbox_vnet.name
-  address_prefixes     = ["10.193.0.0/16"]
+  address_prefixes     = ["10.192.0.0/14"]
 }
 
+resource "azurerm_route_table" "example" {
+  name                          = "sandboxSecurityGroup"
+  location                      = var.location
+  resource_group_name           = azurerm_resource_group.aks-sandbox.name
+  disable_bgp_route_propagation = false
+}
